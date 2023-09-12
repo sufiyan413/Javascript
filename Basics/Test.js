dfed321e717d1887583740d31aac57c45b13eb4a
sufiyan413 <sss413501@gmail.com>
2023-09-12 23:35:42 +0530

    Learned Console

---
+++ Basics/Test.js
@@ -0,0 +1,5 @@
+// console.log is a fundamental method in js used for debugging and printing information
+
+console.log("Hello World");
+console.log("My Name Is Sufiyan");
+console.log("I Am Frontend Developer");
