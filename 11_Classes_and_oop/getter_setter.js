5b60fa121533ad9dae3a660c82005574b439beb9
sufiyan <132542547+sufiyan413@users.noreply.github.com>
2023-10-29 00:32:43 +0530

    learn getters and setters

---
+++ 11_Classes_and_oop/getter_setter.js
@@ -0,0 +1,23 @@
+class User {
+    constructor(email,password){
+      this.email = email;
+      this.password= password
+    }
+
+    get email (){
+        return this._email.toUpperCase()
+    }
+    set email (value){
+        this._email = value
+    }
+
+    get password (){
+       return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password = value
+    }
+}
+
+const Sufiyan = new User("sss@gmail.com","abc")
+console.log(Sufiyan.email);
